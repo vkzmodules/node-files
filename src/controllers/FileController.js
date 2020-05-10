40a50f3c8afaf59634fd5f1ba36daa5a6dbfc762
vKobilarz <vkobilarz@gmail.com>
2020-05-10 15:05:57 -0300

    first commit

---
+++ src/controllers/FileController.js
@@ -0,0 +1,15 @@
+class FileController {
+  constructor(model) {
+    this.File = model
+  }
+
+  store = async (req, res) => {
+    const { originalname: name, filename: path } = req.file
+
+    const file = await File.create({ name, path })
+
+    return res.json(file)
+  }
+}
+
+export default FileController
